bd124758e06ea67e7a5d721c11e3e9e8a6f7dca0
lenny4469 <162060373+lenny4469@users.noreply.github.com>
2024-04-11 10:22:19 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution in this file!
+
+const distanceFromHqInBlocks = (blockNumber) => {
+
+    const hqStreetNumber = 42
+
+    let distanceInBlocks = Math.abs(hqStreetNumber - blockNumber)
+
+    return(distanceInBlocks)
+}
+
+const distanceFromHqInFeet = (blockNumber2) => {
+
+    let distanceInFeet = distanceFromHqInBlocks(blockNumber2) * 264
+    
+    return(distanceInFeet)
+}
+
+const distanceTravelledInFeet = (startBlock, endBlock) => {
+
+    let distanceInFeet = Math.abs(startBlock - endBlock) * 264 
+
+    return(distanceInFeet)
+}
+
+const calculatesFarePrice = (start, destination) => {
+
+    let farePrice = 0;
+    let distanceInFeet = Math.abs(start - destination) * 264 
+    
+    
+    if (distanceInFeet <= 400) {
+        farePrice = 0;
+    }
+        else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+            farePrice = (distanceInFeet - 400) * 0.02
+        }
+        else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+            farePrice = 25.00
+        }
+        else if (distanceInFeet > 2500) {
+            farePrice = 'cannot travel that far'
+        }
+
+    return(farePrice)
+
+}
+
